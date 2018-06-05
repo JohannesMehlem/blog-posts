0c03f56184bd57905bce85414688dc453fd90535
JohannesMehlem <35160933+JohannesMehlem@users.noreply.github.com>
2018-06-05 06:44:40 +0100

    Update 3-web-traffic-forecasting-with-exponential-smoothing-and-arima.R

--- 3-web-traffic-forecasting-with-exponential-smoothing-and-arima.R
+++ 3-web-traffic-forecasting-with-exponential-smoothing-and-arima.R
@@ -1,4 +1,4 @@
 ### Accompanying code to the blog post: 
-### https://johannesmehlem.com/blog/web-traffic-forecasting-exponential-smoothing-arima
+### https://johannesmehlem.com/blog/time-series-forecasting-exponential-smoothing-arima
 ### Full code repository: 
-### https://github.com/JohannesMehlem/blog-posts/edit/master/3-web-traffic-forecasting-with-exponential-smoothing-and-arima.R
+### https://github.com/JohannesMehlem/blog-posts/edit/master/3-time-series-forecasting-with-exponential-smoothing-and-arima.R
